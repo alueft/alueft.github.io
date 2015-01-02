25731f7c1ba4ebff2c5b1579e3531a6f02241bf1
Angus Lim <anguslim94@gmail.com>
2015-01-01 18:32:54 -0800

    Change "Other" grouping logic
    Details:
    Users are now sorted by frequency immediately, and the "Other" group is
    populated by users, one by one, until 10% of the current pie chart is
    reached. Then, if there would be more than 21 users being shown (not
    including "Other"), more users are added to "Other" until the 21-user
    threshold is reached. Finally, if there would only be one user in
    "Other", it's taken out.

--- generate_users.js
+++ generate_users.js
@@ -1,4 +1,4 @@
-// aggregate <2% users, based on threshold, into a single "Other" group
+// aggregate small frequency users, based on threshold, into a single "Other" group
 // (re)populates user_num_array, graphed_users, and other_msg
 buttlyzer.generate_users = function(threshold) {
   if (buttlyzer.user_num_array_list.length === buttlyzer.user_num_array_ind) {
@@ -7,39 +7,58 @@ buttlyzer.generate_users = function(threshold) {
     buttlyzer.user_num_array = [];
     var other_size = 0;
     var other_last = "";
+    var cur_users = [];
     for (var user in buttlyzer.user_num) {
       if (buttlyzer.user_num.hasOwnProperty(user) && !buttlyzer.graphed_users.hasOwnProperty(user)) {
-        if (buttlyzer.user_num[user] / threshold > 0.02) {
-          buttlyzer.user_num_array.push({name: user, y: buttlyzer.user_num[user], drilldown: true});
-          buttlyzer.graphed_users[user] = buttlyzer.user_num[user];
-        }
-        else {
-          buttlyzer.other_msg += buttlyzer.user_num[user];
-          other_size++;
-          other_last = user;
-        }
+        cur_users.push([buttlyzer.user_num[user], user]);
       }
     }
-
-    // if there is only one user in the "Other" group, graph the user normally
-    if (other_size === 1) {
-      buttlyzer.user_num_array.push({name: other_last, y: buttlyzer.other_msg, drilldown: true});
-      buttlyzer.graphed_users[other_last] = buttlyzer.other_msg;
-      buttlyzer.other_msg = 0;
-    }
-
-    // sort by decreasing number of messages and add "Other" at the end
-    buttlyzer.user_num_array.sort(function(a, b) {
-      if (a.y > b.y) {
+    // sort current users by decreasing frequency
+    cur_users.sort(function(a, b) {
+      if (a[0] > b[0]) {
         return -1;
       }
-      if (a.y < b.y) {
+      if (a[0] < b[0]) {
         return 1;
       }
       return 0;
     });
-    if (buttlyzer.other_msg > 0) {
-      buttlyzer.user_num_array.push({name: "Other", y: buttlyzer.other_msg, drilldown: true});
+
+    var other_users = [];
+    var other_total = 0;
+    var other_ind = cur_users.length;
+
+    // add to users until 10% exceeded
+    for (var i = other_ind-1; i >= 0; i--) {
+      if ((other_total + cur_users[i][0]) / threshold <= 0.1) {
+        other_total += cur_users[i][0];
+        other_ind--;
+      }
+      else {
+        break;
+      }
+    }
+
+    // ensure there will be at most 21 users graphed, not including other
+    while (other_ind > 21) {
+      other_total += cur_users[other_ind-1][0];
+      other_ind--;
+    }
+
+    // if other group contains one user, don't bother with it
+    if (other_ind === cur_users.length-1) {
+      other_total = 0;
+      other_ind++;
+    }
+
+    buttlyzer.other_msg = other_total;
+    for (var i = 0; i < other_ind; i++) {
+      var user = cur_users[i];
+      buttlyzer.user_num_array.push({name: user[1], y: user[0], drilldown: true});
+      buttlyzer.graphed_users[user[1]] = user[0];
+    }
+    if (other_ind < cur_users.length) {
+      buttlyzer.user_num_array.push({name: "Other", y: other_total, drilldown: true});
     }
 
     buttlyzer.user_num_array_list.push(buttlyzer.user_num_array);
